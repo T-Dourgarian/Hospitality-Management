35b95b438b4b506e6eae4b579c5d61cccb62cd5e
Thomas Dourgarian <thomasdourgarian@gmail.com>
2023-04-02 17:12:14 -0500

    notes page and create note dialog

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -6,6 +6,10 @@ import {
     Card,
     Chip,
     Divider,
+    Dialog,
+    DialogContent,
+    DialogActions,
+    DialogTitle,
     Grid,
     Paper,
     TableRow,
@@ -21,6 +25,7 @@ import {
     Box,
     Typography
 } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 import RoomTypeForcast from './RoomTypeForcast';
 
 import RoomList from './RoomList';
@@ -30,62 +35,107 @@ function Notes({ notes }) {
 
 
     const [notesLocal, setNotesLocal] = useState(notes);
+    const [createDialog, setCreateDialog] = useState(false);
 
-      useEffect(() => {
-        // console.log('notes', notes);
-      },[])
+
+    useEffect(() => {
+    // console.log('notes', notes);
+    },[])
+
+    const createDialogToggle = () => {
+        setCreateDialog(!createDialog)
+    }
       
   
 
     return (
-        <Card variant="outlined">
-            <Grid container direction="column" pt={2}>
-                {/* <Grid item xs={8}>
-                    <TextField
-                        id="outlined-textarea"
-                        label="New Note"
-                        multiline
-                        fullWidth
-                        rows={3}
-                    />
+        <Grid container direction="column">
+            <Grid container justifyContent={'space-between'}>
+                <Grid item>
+                    Notes
                 </Grid>
                 <Grid item>
-                    <Button 
-                        // onClick={handleCheckIn}               
+                    <Button
                         variant="contained"
-                    >Add Note</Button>
-                </Grid> */}
+                        onClick={createDialogToggle}
+                    >
+                        <AddIcon />
+                    </Button>
+                </Grid>
+            </Grid>
+            <Card variant="outlined">
+                <Grid container direction="column" pt={2}>
 
-                {
-                    notesLocal[0] && notesLocal.map(note => 
-                        <Grid key={note.f1.id} item py={1} px={1} borderBottom='1px solid grey'>
-                            <Grid container direction="row" >
-                                <Grid item xs={10}>
-                                    { note.f1.text }
-                                </Grid>
-                                <Grid item xs={2} textAlign='center'>
-                                    <Box
-                                        pb={1}
-                                    >
-                                        <Chip size="small" label={note.f2.last_name} variant="outlined" />
-                                    </Box>
-                                    <Box 
-                                        
-                                        sx={{
-                                            fontSize:'12px',
-                                            color: 'grey',
-                                        }}
-                                    >
-                                        { note.f1.created_at.split('T')[0] }
-                                    </Box>
+                    {
+                        notesLocal && notesLocal.map(note => 
+                            <Grid key={note.f1.id} item py={1} px={1} borderBottom='1px solid grey'>
+                                <Grid container direction="row" >
+                                    <Grid item xs={10}>
+                                        { note.f1.text }
+                                    </Grid>
+                                    <Grid item xs={2} textAlign='center'>
+                                        <Box
+                                            pb={1}
+                                        >
+                                            <Chip size="small" label={note.f2.last_name} variant="outlined" />
+                                        </Box>
+                                        <Box 
+                                            
+                                            sx={{
+                                                fontSize:'12px',
+                                                color: 'grey',
+                                            }}
+                                        >
+                                            { note.f1.created_at.split('T')[0] }
+                                        </Box>
+                                    </Grid>
                                 </Grid>
                             </Grid>
-                        </Grid>
-                    ) 
-                }
+                        ) 
+                    }
+                        
+                </Grid>
+            </Card>
+
+
+            <Dialog 
+                open={createDialog} 
+                onClose={createDialogToggle}
+                fullWidth
+            >
+                
+                <DialogContent >
+                    <TextField
+                        autoFocus
+                        label="New Note"
+                        fullWidth
+                        rows={3}
+                        multiline
+                    />
                     
-            </Grid>
-        </Card>
+                </DialogContent>
+                <DialogActions>
+                    <Grid container justifyContent={'space-between'}>
+                    
+                        <Grid item px={2}>
+                            <Button 
+                                variant='outlined'
+                                onClick={createDialogToggle}
+                            >Cancel</Button>
+                        </Grid>
+
+                        <Grid item px={2}>
+                            <Button 
+                                variant='contained'
+                                onClick={createDialogToggle}
+                            >Add +</Button>
+                        </Grid>
+                    </Grid>
+                </DialogActions>
+            </Dialog>
+
+
+        </ Grid>
     );
   }
   
